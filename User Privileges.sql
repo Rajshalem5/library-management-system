b82ad65bfabfaaf7028f53a3a73b62d84b8eb83a
Shalem Raj <106602149+Rajshalem5@users.noreply.github.com>
2024-04-17 20:43:57 +0530

    Update User Privileges.sql

--- User Privileges.sql
+++ User Privileges.sql
@@ -5,18 +5,18 @@
  select Host,user 
  from mysql.user;
  
- GRANT All ON librarysystem.* 
+ GRANT All ON lib_database.* 
  TO 'shalem'@'localhost' 
  WITH GRANT OPTION;
 
 SELECT book.title,book.publisher_id,issue_status.staus 
-FROM librarysystem.book 
-RIGHT JOIN librarysystem.issue_status 
+FROM lib_database.book 
+RIGHT JOIN lib_database.issue_status 
 ON book.book_id = issue_status.book_id;
 
 CREATE ROLE Owner;
 
-GRANT ALL ON librarysystem.* 
+GRANT ALL ON lib_database.* 
 TO Owner;
 
 GRANT Owner to 'shalem'@'localhost';
