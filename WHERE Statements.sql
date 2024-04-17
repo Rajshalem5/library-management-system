c203b26be98046958efd5fb34cbfc400f28260e0
Shalem Raj <106602149+Rajshalem5@users.noreply.github.com>
2024-04-17 22:33:36 +0530

    Update WHERE Statements.sql

--- WHERE Statements.sql
+++ WHERE Statements.sql
@@ -1,6 +1,6 @@
 SELECT book_issued AS "Book", name AS "Customer", address 
 FROM customer 
-WHERE (branch_id=265 OR branch_id=266) AND address LIKE "%BL%";
+WHERE (branch_id=2 OR branch_id=5) AND address LIKE "%BL%";
 
 SELECT category,publisher_id AS "ID",publish_year AS "YEAR" 
 FROM publisher 
@@ -13,9 +13,9 @@ LIKE "_e%";
 
 SELECT name AS EMPLOYEES,branch_id AS BRANCH 
 FROM staff 
-WHERE salary BETWEEN 2500 AND 5500;
+WHERE salary BETWEEN 2500 AND 90000;
 
-SELECT title,author_name AS AUTHOR 
+SELECT author_id,author_name AS AUTHOR 
 FROM author 
-WHERE title 
+WHERE author_name
 LIKE "%s%";
