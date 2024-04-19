234016cba3041ffb37995acc580cc4a0abb07530
Shalem Raj <106602149+Rajshalem5@users.noreply.github.com>
2024-04-19 14:05:50 +0530

    Update Manipulating(Insertion) Statements.sql

--- Manipulating(Insertion) Statements.sql
+++ Manipulating(Insertion) Statements.sql
@@ -29,3 +29,18 @@ VALUE(6,269,"BLOCK-20,F-SITE");
 INSERT 
 INTO issue_status 
 VALUE(19,200701,"returned",7,111);
+
+//PART2
+
+INSERT INTO Review (book_id, customer_id, rating, review_text, review_date)
+VALUES (2, 3, 4, 'Captivating storyline!', CURDATE());
+
+INSERT INTO Library_Card (customer_id, issue_date, expiry_date)
+VALUES (4, CURDATE(), DATE_ADD(CURDATE(), INTERVAL 1 YEAR));
+
+INSERT INTO Fines (customer_id, book_id, issue_id, fine_amount, due_date)
+VALUES (2, 3, 4, 10.00, DATE_ADD(CURDATE(), INTERVAL 14 DAY));
+
+INSERT INTO Subscription (customer_id, subscription_type, start_date, end_date)
+VALUES (3, 'Annual', CURDATE(), DATE_ADD(CURDATE(), INTERVAL 1 YEAR));
+
